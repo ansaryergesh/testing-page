36f58c3b84662c0261dc4edf4a2f7ccb8b339ca4
ansaryergesh <yergeshovansar@gmail.com>
2020-09-28 15:33:31 +0600

    testing

---
+++ script.js
@@ -0,0 +1,15 @@
+$('#draggable-point').draggable({
+    axis: 'x',
+    containment: "#money-progress"
+  });
+  
+  $('#draggable-point').draggable({
+    drag: function() {
+      var offset = $(this).offset();
+      var xPos = (100 * parseFloat($(this).css("left"))) / (parseFloat($(this).parent().css("width"))) + "%";
+
+      $('#money-progress-bar').css({
+        'width': xPos
+      });
+    }
+  });
